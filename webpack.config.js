45649eca3441a16ace378f3202e93c31c8272036
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-09 11:03:36 +0100

    Migrate from browserify to webpack

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const scripts = process.argv.includes('-p')
+    ? [ // Production CDN links
+        'https://unpkg.com/react@16.6.1/umd/react.production.min.js',
+        'https://unpkg.com/react-dom@16.6.1/umd/react-dom.production.min.js',
+        'https://unpkg.com/@material-ui/core@3.3.2/umd/material-ui.production.min.js',
+        'https://unpkg.com/p2p-lobby/dist/bundle.min.js',
+    ] : [ // Development CDN links (makes building faster)
+        'https://unpkg.com/react@16.6.1/umd/react.development.js',
+        'https://unpkg.com/react-dom@16.6.1/umd/react-dom.development.js',
+        'https://unpkg.com/@material-ui/core@3.3.2/umd/material-ui.development.js',
+        'https://unpkg.com/p2p-lobby@0.0.14/dist/bundle.js',
+    ]
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: [
+            './src/index.tsx',
+            'webpack-hot-middleware/client',
+        ],
+    },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'p2p-lobby': 'p2p',
+        '@material-ui/core': 'window["material-ui"]',
+        // '@material-ui/icons': 'material-icons',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // Required for 'html-webpack-template'
+            inject: false,
+            template: require('html-webpack-template'),
+
+            title: 'Sets the Game',
+            mobile: true,
+            appMountId: 'app',
+            lang: 'en',
+            scripts,
+            links: [
+                "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
+                "https://fonts.googleapis.com/icon?family=Material+Icons",
+            ],
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
