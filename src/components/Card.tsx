d0efbf52155495523e7192939ccf2d51b09a6932
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-10 11:38:19 +0100

    Refactor CSS of cards to use styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,7 @@ import { Card, Details } from 'sets-game-engine'
 
 interface Props {
     card: Card
+    classes: any
     onSelect?: Function
     onUnselect?: Function
 }
@@ -12,26 +13,31 @@ interface State {
     selected: boolean
 }
 
-const CardColor = 'white'
-
-const Square = ({size = 1, color = 'black', opacity = 1}) =>
-    <div style={{
-        border: `0.5em solid ${color}`,
-        borderRadius: '0.25em',
-        display: 'inline-block',
-        overflow: 'hidden',
+// TODO merge similar styles
+const styles = (theme: any) => ({
+    card: {
+        height: '100%',
+        width: '100%',
+    },
+    square: {
         margin: '.5em',
-    }}>
-        <div style={{
-            width: `${size}em`,
-            height: `${size}em`,
-            backgroundColor: color,
-            opacity,
-        }} />
-    </div>
-
-const Triangle = ({size = 1, color = 'black', opacity = 1}) =>
-    <div style={{
+        display: 'inline-block',
+        borderStyle: 'solid',
+        borderWidth: '0.5em',
+        overflow: 'hidden',
+        
+        borderRadius: '0.25em',
+    },
+    circle: {
+        margin: '.5em',
+        display: 'inline-block',
+        borderStyle: 'solid',
+        borderWidth: '0.5em',
+        overflow: 'hidden',
+        
+        borderRadius: '50%',
+    },
+    triangle: {
         position: 'relative',
         display: 'inline-block',
         margin: '.5em',
@@ -39,38 +45,20 @@ const Triangle = ({size = 1, color = 'black', opacity = 1}) =>
         width: 0,
         borderStyle: 'solid',
         borderColor: 'transparent',
-        borderBottomColor: color,
+    },
+    triangleInner: {
+        position: 'absolute',
+        top: '.5em',
 
-        // Equalateral triangle. Thanks Amima :)
-        borderBottomWidth: `${ size + 0.5 }em`,
-        borderRightWidth:  `${(size + 0.5) * Math.sqrt(3) / 3}em`,
-        borderLeftWidth:   `${(size + 0.5) * Math.sqrt(3) / 3}em`,
-    }}>
-        <span style={{
-            position: 'absolute',
-            opacity: 1 - opacity,
-            top: '.5em',
-            left: `${-(size - 0.5) * Math.sqrt(3) / 3}em`,
+        width: 0,
+        borderStyle: 'solid',
+        borderColor: 'transparent',
+        borderBottomColor: theme.palette.background.paper,
+    }
+})
 
-            width: 0,
-            borderStyle: 'solid',
-            borderColor: 'transparent',
-            borderBottomColor: CardColor,
-
-            borderBottomWidth: `${ size - 0.5 }em`,
-            borderRightWidth:  `${(size - 0.5) * Math.sqrt(3) / 3}em`,
-            borderLeftWidth:   `${(size - 0.5) * Math.sqrt(3) / 3}em`,
-        }} />
-    </div>
-
-const Circle = ({size = 1, color = 'black', opacity = 1}) =>
-    <div style={{
-        border: `0.5em solid ${color}`,
-        borderRadius: '50%',
-        display: 'inline-block',
-        overflow: 'hidden',
-        margin: '.5em',
-    }}>
+const Square = ({size = 1, color = 'black', opacity = 1, classes = {} as any}) =>
+    <div style={{borderColor: color}} className={classes.square}>
         <div style={{
             width: `${size}em`,
             height: `${size}em`,
@@ -79,7 +67,35 @@ const Circle = ({size = 1, color = 'black', opacity = 1}) =>
         }} />
     </div>
 
-export default class CardUI extends React.PureComponent<Props, State> {
+const Triangle = ({size = 1, color = 'black', opacity = 1, classes = {} as any}) =>
+    <div className={classes.triangle} style={{
+        borderBottomColor: color,
+
+        // Equalateral triangle. Thanks Amima :)
+        borderBottomWidth: `${ size + 0.5 }em`,
+        borderRightWidth:  `${(size + 0.5) * Math.sqrt(3) / 3}em`,
+        borderLeftWidth:   `${(size + 0.5) * Math.sqrt(3) / 3}em`,
+    }}>
+        <span className={classes.triangleInner} style={{
+            opacity: 1 - opacity,
+            left:              `${-(size - 0.5) * Math.sqrt(3) / 3}em`,
+            borderBottomWidth: `${ size - 0.5 }em`,
+            borderRightWidth:  `${(size - 0.5) * Math.sqrt(3) / 3}em`,
+            borderLeftWidth:   `${(size - 0.5) * Math.sqrt(3) / 3}em`,
+        }} />
+    </div>
+
+const Circle = ({size = 1, color = 'black', opacity = 1, classes = {} as any}) =>
+    <div style={{borderColor: color}} className={classes.circle}>
+        <div style={{
+            width: `${size}em`,
+            height: `${size}em`,
+            backgroundColor: color,
+            opacity,
+        }} />
+    </div>
+
+class CardUI extends React.PureComponent<Props, State> {
 
     private static readonly SHAPES = {
         [Details.Shape.TRIANGLE]: Triangle,
@@ -108,8 +124,8 @@ export default class CardUI extends React.PureComponent<Props, State> {
     }
 
     render = () =>
-        <Grid item xs={4}>
-            <MaterialCard onClick={this.toggle} raised={this.state.selected} style={{height: '100%'}}>
+        <Grid item sm={4} xs={12}>
+            <MaterialCard onClick={this.toggle} raised={this.state.selected} className={this.props.classes.card}>
                 <CardContent style={{
                     textAlign: 'center',
                     minHeight: '3em',
@@ -121,9 +137,12 @@ export default class CardUI extends React.PureComponent<Props, State> {
                             size={3}
                             color={CardUI.COLORS[this.props.card.color]}
                             opacity={this.props.card.opacity / 2}
+                            classes={this.props.classes as any}
                         />
                     })}
                 </CardContent>
             </MaterialCard>
         </Grid>
 }
+
+export default withStyles(styles as any)(CardUI)
