fefba24e6a34c3ae386338d7388a9fd3873d1227
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-12 12:02:37 -0800

    Add a base for displaying the game. TODO connect peers to room and ready up

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,49 +1,100 @@
 import * as React from 'react'
-import { Button, Typography, TextField, Grid } from '@material-ui/core'
-import { Player } from 'sets-game-engine'
+import { Button, TextField, Grid, CircularProgress } from '@material-ui/core'
+import P2P, { Events } from 'p2p-lobby'
+
+export interface ReadyOpts {
+    players: number
+    rng: (max: number) => number
+    onTake: (set: [number, number, number]) => void
+}
 
 interface Props {
     package: string
-    onReady: (players: Player[]) => void
+    onReady: (opts?: ReadyOpts) => void
+    onError?: (error: Error) => void
 }
 
 interface State {
     name: string
+    loading: boolean
+    online: boolean
+    peers: [string, string][],
 }
 
 export default class Menu extends React.Component<Props, State> {
 
     state: State = {
-        name: ''
+        name: '',
+        loading: false,
+        online: false,
+        peers: [],
     }
 
-    startSolo = () => {
-        this.props.onReady([new Player])
+    private node?: P2P<string>
+
+    private startOnline = async (e: React.FormEvent) => {
+        if(this.canPlayOnline) {
+            e.preventDefault()
+            this.setState({loading: true})
+
+            this.node = new P2P(this.state.name, this.props.package, {allowSameBrowser: true})
+            this.node.on(Events.error, (err: Error) => this.props.onError && this.props.onError(err))
+            this.node.on(Events.peerChange, this.updatePeers)
+            await this.node.joinLobby()
+
+            this.setState({loading: false, online: true})
+        }
     }
+
+    private get canPlayOnline() {
+        return !this.state.loading && this.state.name.trim().length >= 2
+    }
+
+    private updatePeers = () => setTimeout(()=>this.setState({peers: [...this.node!.peers]}), 0)
 
     render() {
-        return <>
+        return this.state.online
+        ? // Online Lobby
+        <pre style={{textAlign: 'left'}}>{JSON.stringify(this.state, null, 2)}</pre>
+        : // Main Menu
+        <> 
             <Grid item container justify="center" sm={6}>
                 <Button
                     variant="contained"
                     color="primary"
                     size="large"
-                    onClick={this.startSolo} >
+                    onClick={() => this.props.onReady()} >
                     Play Solo
                 </Button>
             </Grid>
             <Grid item container justify="center" sm={6}>
-                <TextField
-                    label="Name"
-                    value={this.state.name}
-                    onChange={event => this.setState({name: event.target.value})}
-                    variant="outlined" />
-                <Button
-                    variant="contained"
-                    color="primary"
-                    disabled={this.state.name.trim().length < 2} >
-                    Play with People
-                </Button>
+                <form onSubmit={this.startOnline} noValidate autoComplete="off">
+                    <TextField
+                        label="Name"
+                        value={this.state.name}
+                        onChange={event => this.setState({name: event.target.value})}
+                        variant="outlined"
+                        style={{marginBottom: '1em'}} />
+                    <br/>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        size="large"
+                        type="submit"
+                        disabled={!this.canPlayOnline} >
+                            Play with People
+                            {this.state.loading &&
+                                <CircularProgress
+                                    size={24}
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        left: '50%',
+                                        marginTop: -12,
+                                        marginLeft: -12,
+                                    }} />}
+                    </Button>
+                </form>
             </Grid>
         </>
     }
