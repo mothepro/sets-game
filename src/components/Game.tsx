c70ade1e532e11e3e8eff0b7503becb1433217df
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-12 11:41:24 -0800

    Add support for showing bans to the user and banning

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,60 +1,74 @@
 import * as React from 'react'
 import { Player, Game, Card, Events } from 'sets-game-engine'
-import { Grid, Button, Icon, Typography } from '@material-ui/core'
+import { Grid, Button, Icon, Typography, CircularProgress } from '@material-ui/core'
 import Clock from './Clock'
 import CardUI from './Card'
 
-export type SetIndexs = [number, number, number]
+type index = number
+export type SetIndexs = [index, index, index]
 
 interface Props {
-    rng?: (max: number) => number
-    players?: Player[]
+    players: number
+    rng: (max: number) => number
     
     onTakeAttempt?: (selected: SetIndexs) => void
-    onToggle?: (index: number) => void
-    onEvent?: <K extends Events, V extends typeof Events[K]>(event: K, arg: V) => void
+    onToggle?: (index: index) => void
 }
 
 interface State {
-    selected: number[]
     cards: ReadonlyArray<Card>
+    selected: index[] // indexs of the cards in state
+    bans: { [playerIndex: number]: number } // how far the player has progressed on their ban
 }
 
 export default class GameUI extends React.Component<Props, State> {
-    private game = new Game({
+
+    private readonly game = new Game({
         rng: this.props.rng,
         shoe: 2,
+        timeout: 1000,
+        nextTimeout: (oldTimeout: number) => 2 * oldTimeout
     })
+    private readonly players: Player[] = []
+    private mainPlayer?: Player // will be known at mount time
+    private banHandles: WeakMap<Player, number> = new WeakMap
 
-    state: State = {
-        selected: [],
+    readonly state: State = {
         cards: [],
+        selected: [],
+        bans: {},
     }
 
     /** Defualts for single player mode */
-    protected static defaultProps = { 
-        players:       [new Player],
-        rng:           (max: number) => Math.floor(Math.random() * max),
-        onTakeAttempt: (set: SetIndexs) => GameUI.defaultProps.players[0].takeSet(...set),
+    static defaultProps = {
+        players: 1,
+        rng: (max: number) => Math.floor(Math.random() * max),
     }
 
     componentDidMount() {
-        for(const player of this.props.players!)
+        for(let i = 0; i < this.props.players; i++) {
+            let player = new Player
+            this.players.push(player)
             this.game.addPlayer(player)
-        if(this.props.onEvent)
-            for(const event of this.game.eventNames())
-                this.game.on(event as any, (arg: any) => this.props.onEvent!(event as any, arg))
+        }
+
+        this.game.on(Events.playerBanned, this.onBan)
+        this.game.on(Events.playerUnbanned, this.onUnban)
+        this.game.on(Events.start, this.onStart)
+
         this.game.start()
-        this.setState({cards: this.game.playableCards})
     }
 
+    /** Main player tries to take a set */
     private takeSet = () => {
+        this.mainPlayer!.takeSet(...this.state.selected as SetIndexs)
         if(this.props.onTakeAttempt)
             this.props.onTakeAttempt(this.state.selected as SetIndexs)
         this.setState({selected: []})
     }
 
-    private toggleCard = (index: number) => {
+    /** Main player toggles a card */
+    private toggleCard = (index: index) => {
         const { selected } = this.state
         if(selected.includes(index))
             selected.splice(selected.indexOf(index), 1)
@@ -65,7 +79,55 @@ export default class GameUI extends React.Component<Props, State> {
         this.setState({selected})
     }
 
-    render = () => <>
+    /** Game is ready */
+    private onStart = () => {
+        this.mainPlayer = this.players[0]
+        this.setState({cards: this.game.playableCards})
+    }
+
+    /** When any player is banned */
+    private onBan = ({player, timeout} : {player: Player, timeout: number}) => {
+        // clear original timer if they are somehow banned already
+        if (this.banHandles.has(player))
+            clearInterval(this.banHandles.get(player))
+
+        const index = this.players.indexOf(player),
+              startTime = Date.now()
+
+        this.setState({bans: { [index]: 1 } })
+
+        this.banHandles.set(
+            player,
+            setInterval(
+                () => this.state.bans[index] && // premature unban
+                    this.setState({ bans: { [index]: (Date.now() - startTime) / timeout * 100 } }),
+                Math.ceil(Math.max(timeout / 100, 1000 / 60)) // try to update frame or every whole percent
+            ) as unknown as number
+        )
+    }
+
+    /** When any player is unbanned */
+    private onUnban = (player: Player) => {
+        const index = this.players.indexOf(player)
+        const { bans } = this.state
+        delete bans[index]
+        clearInterval(this.banHandles.get(player))
+        this.banHandles.delete(player)
+        this.setState({bans})
+    }
+
+    render = () => !this.mainPlayer
+        ? // Waiting for game to start
+            <CircularProgress
+            size={50}
+            style={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                marginTop: -25,
+                marginLeft: -25,
+            }} />
+        : <>
             {this.game.playableCards.map((card, i) =>
                 <CardUI
                     key={i}
@@ -79,20 +141,33 @@ export default class GameUI extends React.Component<Props, State> {
                         variant="contained"
                         color="primary"
                         aria-label="Take Set"
-                        disabled={this.state.selected.length != 3}
+                        disabled={this.state.selected.length != 3 || !!this.state.bans[0]}
                         onClick={this.takeSet}
                         size="large"
                     >
                         <Icon style={{margin: '.5em 1em .5em 0'}}>done_outline</Icon>
                         Take Set
+                        {this.state.bans[0] && // main player is banned
+                            <CircularProgress
+                                size={24}
+                                variant="determinate"
+                                color="secondary"
+                                value={this.state.bans[0]}
+                                style={{
+                                    position: 'absolute',
+                                    top: '50%',
+                                    left: '50%',
+                                    marginTop: -12,
+                                    marginLeft: -12,
+                                }} />}
                     </Button>
                 </Grid>
                 <Grid item sm>
-                    {this.props.players!.length == 1
+                    {this.props.players == 1 // solo
                         ? <Typography variant="h5">
-                            {this.props.players![0].score == 0
+                            {this.mainPlayer.score == 0
                                 ? 'You have not collected any sets yet.' 
-                                : `You have collected ${this.props.players![0].score} set${this.props.players![0].score > 1 ? 's' : ''}.`}
+                                : `You have collected ${this.mainPlayer.score} set${this.mainPlayer.score > 1 ? 's' : ''}.`}
                         </Typography>
                         : false }
                 </Grid>
