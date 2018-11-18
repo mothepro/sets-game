d6be07c486c9ccde4d30bda0dcb8c1bd07ff3b19
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2018-11-18 08:33:23 -0800

    Clock can show times greater than an hour

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Typography, Button, Icon, Grid, IconButton } from '@material-ui/core';
+import { Typography, Icon, IconButton } from '@material-ui/core';
 
 interface State {
     start: Date
@@ -9,39 +9,49 @@ interface State {
 
 function dateDifference(start: Date, end: Date): string {
     const diff = (end.getTime() - start.getTime()) / 1000 // don't care about ms
-    return `${Math.floor(diff / 60).toString()}:${Math.floor(diff % 60).toString().padStart(2, '0')}`
+
+    const hours = Math.floor(diff / 60 ** 2),
+        minutes = Math.floor(diff / 60) % 60,
+        seconds = Math.floor(diff % 60)
+
+    return `${hours > 0 ? hours + ':' : ''
+        }${hours > 0 ? minutes.toString().padStart(2, '0') : minutes
+        }:${seconds.toString().padStart(2, '0')}`
 }
 
 export default class extends React.PureComponent<{}, State> {
     
-    state: State = {
+    readonly state: State = {
         start: new Date,
         current: new Date,
     }
 
-    componentWillUnmount = () => this.state.handle && clearInterval(this.state.handle!)
+    componentWillUnmount() {
+        if (this.state.handle)
+            clearInterval(this.state.handle!)
+    }
 
-    render = () => !!this.state.handle
-            ?
-            <>
-                <IconButton onClick={() => {
-                    clearInterval(this.state.handle)
-                    this.setState({handle: undefined})
-                    }}>
-                    <Icon>timer_off</Icon>
-                </IconButton>
-                <Typography
-                    variant="overline"
-                    style={{fontSize: '2em'}} >
-                    {dateDifference(this.state.start, this.state.current)}
-                </Typography>
-            </>
-            :
-            <IconButton onClick={() => 
-                this.setState({
-                    handle: setInterval(() => this.setState({current: new Date}), 1000 / 2) as unknown as number, // node vs browser
-                    current: new Date, // sync clock asap
-                })}>
-                <Icon>timer</Icon>
+    private handleShow = () =>
+        this.setState({
+            handle: setInterval(
+                () => this.setState({current: new Date}),
+                1000 / 2) as unknown as number, // node vs browser
+            current: new Date, // sync clock asap
+        })
+
+    private handleHide = () => {
+        clearInterval(this.state.handle)
+        this.setState({handle: undefined})
+    }
+
+    render = () =>
+        <>
+            <IconButton onClick={!!this.state.handle ? this.handleHide : this.handleShow}>
+                <Icon>{!!this.state.handle ? 'timer_off' : 'timer'}</Icon>
             </IconButton>
+            {!!this.state.handle &&
+                <Typography variant="overline" style={{fontSize: '2em'}} >
+                    {dateDifference(this.state.start, this.state.current)}
+                </Typography> }
+        </>
 }
