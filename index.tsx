fc5eeb3bb2987fce63b05d4833e4bb95ed4ce604
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-19 11:19:08 -0800

    Move files around a bit

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-import {name, version} from '../package.json'
+import {name, version} from './package.json'
 import * as React from 'react'
 import { render } from 'react-dom'
-import App from './components/App'
+import App from './src/App'
 
 render(
     <App package={`${process.env.NODE_ENV == 'development' ? 'dev-' : ''}${name}@${version}`} />,
