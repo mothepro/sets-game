d2e0f5b21efe92829e47c959a3a714146a761127
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-24 09:02:16 -0800

    Make use of material style creator

--- src/shapes.tsx
+++ src/shapes.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {createStyles, Theme, withStyles} from '@material-ui/core'
+import {createStyles, Theme, withStyles, WithStyles} from '@material-ui/core'
 
 interface Color {
     hue: number
@@ -7,24 +7,22 @@ interface Color {
     lightness: number
 }
 
-interface Props {
+interface Props extends WithStyles<typeof styles> {
     size: number
     opacity: number
     color: Color
-    classes: {
-        shape: string
-    }
 }
 
-const styles = ({palette}: Theme) => createStyles({
+const styles = ({palette, spacing}: Theme) => createStyles({
     shape: {
-        margin: '0 .5em',
+        marginLeft: spacing.unit * 2,
+        marginRight: spacing.unit * 2,
         display: 'inline-block',
         borderStyle: 'solid',
         borderColor: 'transparent',
         position: 'relative',
         width: 0,
-        '& span': {
+        '& span': { // for triangles
             width: 0,
             position: 'absolute',
             borderStyle: 'solid',
@@ -34,14 +32,14 @@ const styles = ({palette}: Theme) => createStyles({
     },
 })
 
+// Equilateral Triangle Ratio. Thanks Amima :)
+const baseByHeight = Math.sqrt(3) / 3
+
 const formatColor = ({hue, saturation, lightness}: Color, opacity = 1) =>
     `hsla(${hue}, ${saturation}%, ${lightness}%, ${opacity})`
 
-const TriangleComponent = ({size, color, opacity, classes}: Props) => {
-    const borderWidth  = size / 6,
-          baseByHeight = Math.sqrt(3) / 3 // Equilateral triangle. Thanks Amima :)
-
-    return <div className={classes.shape} style={{
+const TriangleComponent = ({size, color, opacity, classes}: Props) =>
+    <div className={classes.shape} style={{
                 borderBottomColor: formatColor(color),
                 borderBottomWidth: `${size}em`,
                 borderRightWidth:  `${size * baseByHeight}em`,
@@ -49,14 +47,13 @@ const TriangleComponent = ({size, color, opacity, classes}: Props) => {
             }}>
         <span style={{
             opacity:           1 - opacity,
-            top:               `${ borderWidth }em`,
-            left:              `${-(size - borderWidth * 2) * baseByHeight}em`,
-            borderBottomWidth: `${ (size - borderWidth * 2) }em`,
-            borderRightWidth:  `${ (size - borderWidth * 2) * baseByHeight}em`,
-            borderLeftWidth:   `${ (size - borderWidth * 2) * baseByHeight}em`,
+            top:               `${size / 6}em`, // actual border width
+            left:              `${-(size - size / 3) * baseByHeight}em`,
+            borderBottomWidth: `${ (size - size / 3) }em`,
+            borderRightWidth:  `${ (size - size / 3) * baseByHeight}em`,
+            borderLeftWidth:   `${ (size - size / 3) * baseByHeight}em`,
         }} />
     </div>
-}
 
 const SquareComponent = ({size, color, opacity, classes}: Props) =>
     <div className={classes.shape} style={{
@@ -66,7 +63,7 @@ const SquareComponent = ({size, color, opacity, classes}: Props) =>
         borderColor:     formatColor(color),
         backgroundColor: formatColor(color, opacity),
 
-        borderRadius: '0.25em',
+        borderRadius: '0.1em',
     }} />
 
 const CircleComponent = ({size, color, opacity, classes}: Props) =>
@@ -80,8 +77,7 @@ const CircleComponent = ({size, color, opacity, classes}: Props) =>
         borderRadius: '50%',
     }} />
 
-const Circle = withStyles(styles)(CircleComponent),
-      Square = withStyles(styles)(SquareComponent),
-      Triangle = withStyles(styles)(TriangleComponent)
-
-export { Circle, Triangle, Square }
+export const
+    Circle = withStyles(styles)(CircleComponent),
+    Square = withStyles(styles)(SquareComponent),
+    Triangle = withStyles(styles)(TriangleComponent)
