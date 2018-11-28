a1304b7d2e50b216eaf76d7431881b632e7fde60
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2018-11-28 09:47:16 -0800

    Fix boolean array packer

--- src/messages.ts
+++ src/messages.ts
@@ -5,15 +5,18 @@ import {CardOption} from './Game'
  * The list of selected cards.
  * Since the cards should be synced for all, this is a more compact
  * way to point out the indexes of the card array.
+ * 
+ * We reduce backwards and reverse the number becuase
+ * we don't care about trailing `false`s, only leading ones.
  */
 export class Selection {
     constructor(public selection: CardOption) {}
 
     static pack = (inst: Selection) =>
-        inst.selection.reduce((total, bit) => total << 1 | +bit, 0)
+        inst.selection.reduceRight((total, bit) => total << 1 | +bit, 0)
 
     static unpack = (selected: number) =>
-        new Selection(selected.toString(2).split('').map(bit => bit == '1'))
+        new Selection(selected.toString(2).split('').reverse().map(bit => bit == '1'))
 }
 
 Packer(Selection)
